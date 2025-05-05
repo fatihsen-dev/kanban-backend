a585a6f5be418b7ed3f6cfe84550d5e077207474
Fatih Sen <dev.fatihsen@outlook.com>
2025-05-06 00:46:11 +0300

    feat: update password validation rules in user requests
    - Modified the password validation for both `UserLoginRequest` and `UserRegisterRequest` to enforce a maximum length of 36 characters, ensuring better security and user experience.

--- internal/adapters/driver/http/datatransfers/requests/requests_user.go
+++ internal/adapters/driver/http/datatransfers/requests/requests_user.go
@@ -2,11 +2,11 @@ package requests
 
 type UserLoginRequest struct {
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=8,containsany=!@#$%^&*()?"`
+	Password string `json:"password" validate:"required,min=8,max=36"`
 }
 
 type UserRegisterRequest struct {
 	Name     string `json:"name" validate:"required,min=3,max=26"`
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=8,containsany=!@#$%^&*()?"`
+	Password string `json:"password" validate:"required,min=8,max=36"`
 }
