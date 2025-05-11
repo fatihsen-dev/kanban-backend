27e744299250d5687d3b485e94ad1afe45d1d5ed
fatihsen-dev <fatihydrm@outlook.com>
2025-05-11 16:19:39 +0300

    fix: increase content field validation limit in task request structures
    - Updated the validation rules for the `Content` field in both `TaskCreateRequest` and `TaskUpdateRequest` to allow a maximum length of 1400 characters, enhancing the flexibility for task details.

--- internal/adapters/driver/http/datatransfers/requests/requests_task.go
+++ internal/adapters/driver/http/datatransfers/requests/requests_task.go
@@ -2,13 +2,13 @@ package requests
 
 type TaskCreateRequest struct {
 	Title     string  `json:"title" validate:"required,min=3,max=26"`
-	Content   *string `json:"content,omitempty" validate:"omitempty,max=600"`
+	Content   *string `json:"content,omitempty" validate:"omitempty,max=1400"`
 	ColumnID  string  `json:"column_id" validate:"required,uuid4"`
 	ProjectID string  `json:"project_id" validate:"required,uuid4"`
 }
 
 type TaskUpdateRequest struct {
 	Title    *string `json:"title,omitempty" validate:"omitempty,min=3,max=26"`
-	Content  *string `json:"content,omitempty" validate:"omitempty,max=600"`
+	Content  *string `json:"content,omitempty" validate:"omitempty,max=1400"`
 	ColumnID *string `json:"column_id,omitempty" validate:"omitempty,uuid4"`
 }
